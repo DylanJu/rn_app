aee5ec452627a5cd0bfc3980fe1644c5b5d670fd
Dylan <benefitjoo@paxdaq.com>
2019-02-08 21:06:54 +0900

    Adding basic login page

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,26 +1,76 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const MyBackground = styled.ImageBackground`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const LoginLayer = styled.View`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: black;
+  opacity: 0.2;
+`;
+
+const SocialLoginButton = styled.TouchableHighlight`
+  flex: 1;
+  width: 100px;
+  height: 45px;
+  border-radius: 3px;
+  background-color: #3fffff;
+  border: 1px solid #3fffff;
+`;
+
+const SocialLoginText = styled.Text`
+  color: white;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+`;
+
+// interface Props {
+
+// }
+
+class SocialLoginButtonGroup extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+
+  //   };
+  // }
+  render() {
+    return (
+      <SocialLoginButton>
+        <SocialLoginText>Facebook</SocialLoginText>
+      </SocialLoginButton>
+    )
+  }
+}
 
 export default class SignIn extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>SignIn</Text>
-      </View>
+      <MyBackground source={require('img/sign_in_background.jpg')}>
+        <LoginLayer />
+        <Title>Travel Mate</Title>
+
+        <SocialLoginButton>
+          <SocialLoginText>Facebook</SocialLoginText>
+        </SocialLoginButton>
+
+        <SocialLoginButton>
+          <SocialLoginText>Facebook</SocialLoginText>
+        </SocialLoginButton>
+      </MyBackground>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
